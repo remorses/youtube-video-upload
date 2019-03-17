a68dd57981cda3fe1ac0f98a2247c97c3e39a7ac
remorses <beats.by.morse@gmail.com>
2019-03-17 15:20:05 +0100

    added deploy pypi

---
+++ deploy_pypi.sh
@@ -0,0 +1,13 @@
+
+cd `dirname $0`
+cd ../src
+
+test -f ./VERSION || (echo "file VERSION containing current version is needed" && exit 1)
+
+python3 setup.py sdist bdist_wheel
+
+ls -1
+
+# -u $PYPIUSERNAME -p $PYPIPASSWORD
+
+python3 -m twine upload  dist/*
